74b1477193bf828957654bc13b51094650bc0f89
Olav Bech Bråten <olavbb@hotmail.com>
2019-05-09 20:02:51 +0200

    Add monitor endpoint

--- handlers/usb.go
+++ handlers/usb.go
@@ -5,12 +5,16 @@ import "fmt"
 import "encoding/json"
 import "github.com/google/gousb"
 import "github.com/google/gousb/usbid"
+import "github.com/gorilla/websocket"
 import "github.com/Balonbal/usb-monitor-websocket/usb"
 
 type UsbDesc struct {
 	*gousb.DeviceDesc
 	Description string
 }
+
+var _clients = make(map[*websocket.Conn]bool)
+var _upgrader = websocket.Upgrader{}
 
 func ListUsb(w http.ResponseWriter, r *http.Request, ctx *gousb.Context) {
 	usbs := usb.ListUsbs(ctx)
@@ -22,9 +26,48 @@ func ListUsb(w http.ResponseWriter, r *http.Request, ctx *gousb.Context) {
 		})
 	}
 	mess, _ := json.Marshal(arr)
+	w.Header().Set("Content-Type", "application/json")
 	i, err := w.Write([]byte(mess))
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(i)
+}
+
+func RegisterMonitor(w http.ResponseWriter, r *http.Request, clientCh chan<- *websocket.Conn) {
+	socket, err := _upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	clientCh <- socket
+
+}
+
+func broadcast(eventCh <-chan interface{}, clientCh <-chan *websocket.Conn) {
+	clients := make(map[*websocket.Conn]bool)
+	for {
+		select {
+		case client := <-clientCh:
+			clients[client] = true
+		case event := <-eventCh:
+			message, err := json.Marshal(event)
+			if err != nil {
+				fmt.Println(err)
+			}
+			for client := range clients {
+				err := client.WriteMessage(websocket.TextMessage, message)
+				if err != nil {
+					fmt.Println(err)
+					client.Close()
+					delete(clients, client)
+				}
+			}
+		}
+	}
+}
+
+func InitBroadcaster(eventCh chan interface{}) (chan<- *websocket.Conn) {
+	clientCh := make(chan *websocket.Conn)
+	go broadcast(eventCh, clientCh)
+	return clientCh
 }
--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 
 func main() {
 	usbCtx := gousb.NewContext()
+	usbCh := make(chan interface{})
+	clientCh := handlers.InitBroadcaster(usbCh)
 
-	http.HandleFunc("/listUsbs", func(w http.ResponseWriter, r *http.Request) { handlers.ListUsb(w, r, usbCtx) });
+	http.HandleFunc("/listUsbs", func(w http.ResponseWriter, r *http.Request) { handlers.ListUsb(w, r, usbCtx) })
+	http.HandleFunc("/monitor", func(w http.ResponseWriter, r *http.Request) {
+		handlers.RegisterMonitor(w, r, clientCh)
+	})
 
+	defer usbCtx.Close()
 	panic(http.ListenAndServe(":19123", nil))
 }
